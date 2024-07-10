4f3af2d94b39e79e61673a51d77a112e1deaafe7
sanjana <sanjanaramprasad1@gmail.com>
2024-07-10 16:18:33 -0400

    Add Heroku deployment files

--- app.py
+++ app.py
@@ -226,5 +226,5 @@ def save_annotations():
 
     return jsonify({"success": True})
 
-# if __name__ == "__main__":
-#     application.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=8080)
